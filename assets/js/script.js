82aea05c49bbc0b0721957c7fabe1ddeba51f3b7
Juan Acevedo <jmamartinez18@gmail.com>
2023-11-11 11:01:04 -0700

    adding style to landing page

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var starButton = document.querySelector (".start");
+
+function startQuiz() {
+    showQuestionQuestion()
+    startTimer()
+}
+
+function showQuestion() {
+}
+
+starButton.addEventListener ("click", startQuiz);
