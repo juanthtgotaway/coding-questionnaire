1f8fd9d99996f2e90b1e2ac5e005ee902ec1cc84
Juan Acevedo <jmamartinez18@gmail.com>
2023-11-11 15:23:52 -0700

    added timer to left screen with inline style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,22 @@
 var starButton = document.querySelector (".start");
+var timerEl = document.querySelector(".time");
 
 function startQuiz() {
-    showQuestionQuestion()
     startTimer()
 }
 
-function showQuestion() {
-}
+function startTimer() {
+    var timeLeft = 90;
+  
+    var timeInterval = setInterval(function () {
+      if (timeLeft > 1) {
+        timerEl.textContent = 'Time Left:' + timeLeft;
+        timeLeft--;
+      } else if (timeLeft === 1) {
+        timerEl.textContent = 'Time Left:' + timeLeft;
+        timeLeft--;
+      }
+    }, 1000);
+  }
 
 starButton.addEventListener ("click", startQuiz);
