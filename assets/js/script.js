e6ffced86aa57019a07c99b6d5ef6fb84c084634
Juan Acevedo <jmamartinez18@gmail.com>
2023-11-12 10:53:27 -0700

    have questions show up and timer start when button is pressed also added styles to the buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
 var starButton = document.querySelector (".start");
 var timerEl = document.querySelector(".time");
+var questionBox = document.querySelector(".question-box")
+
+var questionIndex = 0;
 
 function startQuiz() {
     startTimer()
+    showQuestions()
 }
 
 function startTimer() {
@@ -17,6 +21,44 @@ function startTimer() {
         timeLeft--;
       }
     }, 1000);
-  }
+}
+
+var questions = [
+    { question: "Which of the following is part of a URL?", options: ["HTML", "editor", "domain", "host"], correct: "domain" },
+    { question: "What is the correct file for a web page?", options: [".doc", ".ppt", ".txt", ".html"], correct: ".html" },
+    { question: "How do you write a comment?", options: ["//", "**", "..", "cd"], correct: "//"},
+    { question: "Block of code designed to perform a particular task or set of tasks", options: ["Array", "Variable", "Function"], correct: "Function" },
+    { question: "Do you enjoy Javascript?", options: ["YES!🤩", "NO! 🥲"], correct: "NO! 🥲" },
+];
+
+function showQuestions() {
+    if (questions.length > 0) {
+        var currentQuestion = questions[0]; 
+        var content = '<h3>' + currentQuestion.question + '</h3>';
+
+        for (var i = 0; i < currentQuestion.options.length; i++) {
+            var option = currentQuestion.options[i];
+            content += '<button class="option-button" onclick="selectOption(\'' + option + '\')">' + option + '</button>';
+        }
+
+        content += '<div id="feedback"></div>';
+
+        questionBox.innerHTML = content;
+    } else {
+        questionBox.innerHTML = '<h1>Quiz Completed!</h1>';
+    }
+}
+
+function selectOption(selectedOption) {
+    var currentQuestion = questions[0];
+    if (selectedOption === currentQuestion.correct) {
+        console.log("Correct Answer");
+    } else {
+        console.log("Incorrect Answer");
+    }
+    questions.shift(); 
+    showQuestions(); 
+}
+
 
 starButton.addEventListener ("click", startQuiz);
